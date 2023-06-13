60fb0d8381355a5f38aa25ea68ae6908f8fda4f1
03den <03denchik03@gmail.com>
2023-06-13 16:24:52 +0300

    Всё

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
+import java.util.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 999, 999, 3, 7, 0, 4, 4, 5, 5, 6, 7));
+        System.out.println("входной массив " + nums);
+        task1(nums);
+        task2(nums);
+        List<String> words = List.of("hello", "world", "hello", "how are you");
+        task3(words);
+        task4(words);
+    }
+    public static void task1(List<Integer> nums) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                result.add(num);
+            }
+        }
+        System.out.println(result);
+    }
+    public static void task2(List<Integer> nums) {
+        Set<Integer> result = new TreeSet<>(nums);
+        System.out.println(result);
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task3(List<String> words) {
+        Set<String> uniqWords = new TreeSet<>(words);
+        System.out.println(uniqWords);
+    }
+    public static void task4(List<String> words) {
+        Map<String, Integer> wordByCount = new HashMap<>();
+        for (String word : words) {
+            if (wordByCount.containsKey(word)) {
+                wordByCount.put(word, wordByCount.get(word) + 1);
+            } else {
+                wordByCount.put(word, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : wordByCount.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry);
+            }
         }
     }
 }
